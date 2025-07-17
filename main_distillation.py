65a4cd58ebe16d0d20fb98bf59501ca1cb593dbf
Farhan Samir <f.samir@mail.utoronto.ca>
2025-07-17 13:54:08 -0700

    try SFTTrainer

--- main_distillation.py
+++ main_distillation.py
@@ -129,7 +129,7 @@ def distill_task1_olmo():
         max_length=3300,
         output_dir="/h/smfsamir/hf_cache/olmo-1b-hf_task1_distillation",
         eval_steps = 10,
-        evaluation_strategy="steps"
+        eval_strategy="steps"
     )
     #                                            ) 
     trainer = SFTTrainer(
