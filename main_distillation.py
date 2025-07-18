0a7db6576bbb70a42db570f081bf5e13f379ee14
Farhan Samir <f.samir@mail.utoronto.ca>
2025-07-17 17:29:21 -0700

    try custom trainer and dataloader

--- main_distillation.py
+++ main_distillation.py
@@ -154,6 +154,7 @@ class CustomTrainer(Trainer):
 
 def preprocess_text(example):
     batch = OLMO_TOKENIZER(example['prompt'] + example['completion'])
+    ipdb.set_trace()
     batch['labels'] = batch['input_ids'].copy()  # shifting is done in the model
     return batch
 
