23042ab15222212c0bae1fa03552231ea78da3d4
Farhan Samir <f.samir@mail.utoronto.ca>
2025-09-07 16:30:49 -0400

    add iterable import

--- packages/parsing_utils.py
+++ packages/parsing_utils.py
@@ -1,7 +1,7 @@
 import os
 import json
 from dataclasses import dataclass
-from typing import List, Tuple, Optional, Set
+from typing import List, Tuple, Optional, Set, Iterable
 
 @dataclass
 class CorefEntityMetadata:
