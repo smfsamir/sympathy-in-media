94063713a371f6af51c79ea9a53dc2166a47f68c
Farhan Samir <f.samir@mail.utoronto.ca>
2025-09-07 20:26:23 -0400

    add slurm script for finetuning

---
+++ distill.sh
@@ -0,0 +1,12 @@
+#SBATCH --time=2:00:00
+#SBATCH --job-name=distill-flant5-sympathy
+#SBATCH --gres=gpu:rtx_a6000:1 
+#SBATCH --mem=16G
+#SBATCH --mail-type=END,FAIL,INVALID_DEPEND
+#SBATCH --mail-user=f.samir@utoronto.ca
+#SBATCH --output=R-flant5-%x.%j.out
+#SBATCH --error=R-flant5-%x.%j.err
+source ~/sympathy_venv/bin/activate
+cd sympathy-in-media
+
+python main_distillation.py distill-flant5
