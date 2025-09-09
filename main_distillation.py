d2b67646819c72fa1f4ad5464da45e593f8732be
Farhan Samir <f.samir@mail.utoronto.ca>
2025-09-09 13:30:08 -0400

    fix key names

--- main_distillation.py
+++ main_distillation.py
@@ -79,6 +79,7 @@ class CustomSeq2SeqTrainer(Seq2SeqTrainer):
                 all_cer_metrics.extend(batch_metrics['cer']) 
                 all_is_correct_labels.extend(batch_metrics['entity_present_correct'])
             f1_metric_dict = f1_score(all_eval_entity_present_gt_labels, all_eval_entity_present_prediction_labels, average=None)
+            ipdb.set_trace()
             # TODO: get the f1 for the positive class ('valid entity' class)
             accuracy_metric = sum(all_is_correct_labels) / len(all_is_correct_labels) # TODO
             cer_metric = np.median(all_cer_metrics)
