ac474c18e84e0851d96ce3c9f629d4a181759ab6
Farhan Samir <f.samir@mail.utoronto.ca>
2025-09-09 13:43:23 -0400

    remove logging of unrecognized text

--- packages/flan_utils.py
+++ packages/flan_utils.py
@@ -102,6 +102,5 @@ def convert_text_to_entity_present_label(description_texts):
         elif entity_present_str in text.lower():
             labels.append('valid entity')
         else:
-            logger.warning(f"Unrecognized text: {text}") # likely to happen during the early steps of training
             labels.append('unrecognized')
     return labels
