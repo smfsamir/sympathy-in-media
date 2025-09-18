de3a5c78364921abb7e56d64596425d65c0c5fc2
Farhan Samir <f.samir@mail.utoronto.ca>
2025-09-18 10:26:19 +0200

    fix key names

--- main_distillation.py
+++ main_distillation.py
@@ -238,6 +238,7 @@ def assess_ft_flan_model():
                            data_files={'train': "data/distillation_data/coref_training_dataset.json"}, 
                            split='train')
 
+    ipdb.set_trace()
     # rolling_training_dataset.json
     # split train_dataset into train and validation sets
     # eval_subjects = ['Danny Lafrance-Godmer', 'Jeremy Nuvviaq', 'Dale Culver', 'Jason Gary Roy', 'Bradley Thomas Clattenburg', 'Charles Qirngnirq', 'Riley Fairholm', 'Radford James Good Dagger', 'Elgyn Muskego', 'Illutak Anautak', 'Christopher Arkell', 'David Charles Sandaker', 'Abisay Cruz', 'William David McCaffrey', 'John Robert Buehler'] 
