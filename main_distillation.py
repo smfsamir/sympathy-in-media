ce95e738cad633db98628ce0f333bb2c76ceb8c6
Farhan Samir <f.samir@mail.utoronto.ca>
2025-09-20 01:15:30 +0200

    update eval batch size

--- main_distillation.py
+++ main_distillation.py
@@ -250,6 +250,7 @@ def evaluate_proportion_distribution_metric(dataset):
                 whole_article_indices = [paragraphs_ordered.index(para) + 1 for para in paras_extracted if para in paragraphs_ordered]
                 police_aligned = is_police_aligned_entity(coref_prediction_text)
                 paragraph_index_to_assignments.update({index: 'police-aligned' if police_aligned else 'victim-aligned' for index in whole_article_indices})
+                print(paragraph_index_to_assignments)
 
         # Then, obtain their paragraphs.
         # then, map those paragraphs into their index into the original article.
