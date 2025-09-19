6a746c7450a0f84f4c73072b95b2f90114a52c35
Farhan Samir <f.samir@mail.utoronto.ca>
2025-09-20 01:25:25 +0200

    update eval batch size

--- main_distillation.py
+++ main_distillation.py
@@ -247,7 +247,11 @@ def evaluate_proportion_distribution_metric(dataset):
             coref_prediction_text = article_subset['predicted_text'][i]
             if is_valid_entity_present(coref_prediction_text) == 'valid entity':
                 relevant_paragraph_indices = extract_relevant_paragraphs(coref_prediction_text)
-                relevant_paragraphs = [paras_extracted[idx - 1] for idx in relevant_paragraph_indices]
+                try:
+                    relevant_paragraphs = [paras_extracted[idx - 1] for idx in relevant_paragraph_indices]
+                except IndexError:
+                    logger.warning(f"Index error for {article} with indices {relevant_paragraph_indices} and paragraphs {paras_extracted}")
+                    ipdb.set_trace()
                 whole_article_indices = [paragraphs_ordered.index(para) + 1 for para in relevant_paragraphs]
                 police_aligned = is_police_aligned_entity(coref_prediction_text)
                 paragraph_index_to_assignments.update({index: 'police-aligned' if police_aligned else 'victim-aligned' for index in whole_article_indices})
