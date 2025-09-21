3929c419c63aa7c5a6809f6bdb0577a3c096d79d
Farhan Samir <f.samir@mail.utoronto.ca>
2025-09-21 07:24:24 -0400

    remove debug statement

--- packages/flan_utils.py
+++ packages/flan_utils.py
@@ -116,7 +116,6 @@ def is_valid_entity_present(description_text):
 def is_police_aligned_entity(description_text):
     assert is_valid_entity_present(description_text) == 'valid entity', "Entity is not valid"
     if 'not aligned with the police' in description_text.lower():
-        ipdb.set_trace()
         return False
     else:
         return True
