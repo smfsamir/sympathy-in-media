58e2ed7ac2d7205c9112fc0f64a6467506cabd5d
Farhan Samir <f.samir@mail.utoronto.ca>
2025-09-28 10:07:11 -0400

    update model

--- main_distillation.py
+++ main_distillation.py
@@ -234,7 +234,7 @@ def evaluate_proportion_distribution_metric(dataset):
             identifier=index
         )
         paragraphs_ordered = [normalize_whitespace(paragraph) for paragraph in load_article_paragraphs(article)]
-        paras_extracted = extract_enumerated_paragraphs(article_subset['prompt'][0])
+        paras_extracted = [normalize_whitespace(paragraph) for paragraph in extract_enumerated_paragraphs(article_subset['prompt'][0])]
         for para in paras_extracted:
             assert para in paragraphs_ordered, f"Extracted paragraph not in original paragraphs: {para}"
         logger.info(f"Good for {article}")
