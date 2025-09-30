63991e851b219b6f8be993c6f1ae14745df1c2af
Farhan Samir <f.samir@mail.utoronto.ca>
2025-09-30 09:20:42 -0400

    compute coref annotations for unsupervised articles

---
+++ main_unsupervised.py
@@ -0,0 +1,34 @@
+import json
+import os
+import click 
+import loguru
+
+logger = loguru.logger
+from packages.coref_utils import compute_fastcoref_annotation
+
+@click.command()
+def inspect_distributions_unsupervised():
+    pass
+
+@click.command()
+def compute_fastcoref_annotations():
+    from fastcoref import FCoref
+    model = FCoref(device='cuda:0')
+
+    all_articles = os.listdir('unsupervised_articles')
+    fcoref_annotations = []
+    for article in all_articles:
+        with open(f'unsupervised_articles/{article}', 'r') as f:
+            paragraphs = json.load(f)['article']
+        fcoref_annotations.append(
+            compute_fastcoref_annotation(model, paragraphs, article) 
+        )
+    with open('data/unsupervised_fcoref_annotations.json', 'w') as f:
+        json.dump(fcoref_annotations, f, indent=4)
+
+@click.group()
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ packages/coref_utils.py
@@ -0,0 +1,31 @@
+from typing import List
+
+def compute_paragraph_boundaries(paragraphs):
+    paragraph_boundaries =[]
+    start = 0
+    end = 0
+    for i, paragraph in enumerate(paragraphs):
+        if i == len(paragraphs) - 1:
+            end = start + len(paragraph)
+        else: 
+            end = start + len(paragraph) + 1
+        paragraph_boundaries.append((start, end))
+        start = end
+    return paragraph_boundaries
+
+def compute_fastcoref_annotation(fcoref_instance, 
+                                 article_paragraphs: List[str], 
+                                 article_name):
+    full_text = " ".join(article_paragraphs)
+    paragraph_boundaries = compute_paragraph_boundaries(article_paragraphs)
+    preds = fcoref_instance.predict(texts=[full_text])[0]
+    clusters = preds.get_clusters()
+    cluster_indices = preds.get_clusters(as_strings=False)
+    # TODO: have to put the target paragraphs into the clusters.
+    return {
+        'article': article_name,
+        'cluster_indices': cluster_indices,
+        'clusters': clusters,
+        'full_text': full_text,
+        'paragraph_boundaries': paragraph_boundaries
+    }
